de2e98869267fa56b2963c2628c416ffb823df8a
dai-rewahandi <niktodaire@gmail.com>
2025-01-07 12:22:34 +0700

    Add initial project setup with Tailwind CSS and MDX integration

---
+++ src/utils/head.ts
@@ -0,0 +1,2 @@
+export const desc =
+  "Saya Dai, seorang pengembang web front-end dengan minat yang besar pada teknologi terbaru. Saya memiliki pengalaman dalam membangun aplikasi web yang responsif dan scalable menggunakan Astro, React, dan Next.js. Saya selalu bersemangat untuk belajar hal-hal baru dan mengembangkan proyek-proyek yang inovatif.";
